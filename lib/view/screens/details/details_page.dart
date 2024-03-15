ce92a950c93ed0e30ef56c8c7931081e3c260e15
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2024-03-15 23:34:33 +0530

    Remove RefreshIndicator from DetailsPage

--- lib/view/screens/details/details_page.dart
+++ lib/view/screens/details/details_page.dart
@@ -29,63 +29,59 @@ class _DetailsPageState extends State<DetailsPage> {
         child: Consumer<ProductDetailsViewModel>(
           builder: (context, value, _) {
             return Scaffold(
-                appBar: AppBar(
-                  title: Text(value.product.data?.title ?? "",
-                      style: const TextStyle(color: Colors.white)),
-                  centerTitle: true,
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-                body: RefreshIndicator(
-                  onRefresh: () => viewModel.getProductDetails(widget.id),
-                  child: (value.product.status == Status.LOADING)
-                      ? const Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : (value.product.status == Status.SUCCESS)
-                          ? Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  Image.network(
-                                    value.product.data?.thumbnail ?? "",
-                                    height: 300,
-                                    width: 300,
-                                  ),
-                                  Text(value.product.data?.description ?? ""),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                      "Price: \$${value.product.data?.price.toString()}"),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                      "Category: ${value.product.data?.category}"),
-                                  const SizedBox(height: 10),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      ElevatedButton(
-                                          onPressed: () {
-                                            buyProduct();
-                                          },
-                                          child: const Text("Buy Product")),
-                                    ],
-                                  )
-                                ],
+              appBar: AppBar(
+                title: Text(value.product.data?.title ?? "",
+                    style: const TextStyle(color: Colors.white)),
+                centerTitle: true,
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+              body: (value.product.status == Status.LOADING)
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : (value.product.status == Status.SUCCESS)
+                      ? Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Image.network(
+                                value.product.data?.thumbnail ?? "",
                               ),
-                            )
-                          : Center(
-                              child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                  Text(value.product.message.toString()),
+                              const SizedBox(height: 10),
+                              Text(value.product.data?.description ?? ""),
+                              const SizedBox(height: 10),
+                              Text(
+                                  "Price: \$${value.product.data?.price.toString()}"),
+                              const SizedBox(height: 10),
+                              Text("Category: ${value.product.data?.category}"),
+                              const SizedBox(height: 50),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
                                   ElevatedButton(
                                       onPressed: () {
-                                        viewModel.getProductDetails(widget.id);
+                                        buyProduct();
                                       },
-                                      child: const Text("Reload"))
-                                ])),
-                ));
+                                      child: const Text("Buy Product")),
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      : Center(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              Text(value.product.message.toString()),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    viewModel.getProductDetails(widget.id);
+                                  },
+                                  child: const Text("Reload"))
+                            ])),
+            );
           },
         ));
   }
